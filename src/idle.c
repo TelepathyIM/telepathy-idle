089e1d656332d58811ec369fc2147194c05e95d3
Olli Salli <olli.salli@collabora.co.uk>
2007-04-20 10:31:24 +0000

    Use run.h
    20070420103124-9db4d-ab6879b88a63eb9b95192b1cfb035fbdbfec1848.gz

--- src/idle.c
+++ src/idle.c
@@ -18,82 +18,26 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "idle.h"
+#include <config.h>
+
+#include <telepathy-glib/run.h>
+#include <telepathy-glib/debug.h>
 
 #include "idle-connection-manager.h"
 
-#include <stdlib.h>
-#include <stdio.h>
+static TpBaseConnectionManager *_construct_cm() {
+	TpBaseConnectionManager *base_cm = TP_BASE_CONNECTION_MANAGER(g_object_new(IDLE_TYPE_CONNECTION_MANAGER, NULL));
 
-#include <telepathy-glib/errors.h>
-
-GSource *timeout = NULL;
-GMainLoop *mainloop = NULL;
-IdleConnectionManager *manager = NULL;
-gboolean connections_exist = FALSE;
-guint timeout_id;
-
-#define DIE_TIME 5000
-
-static gboolean
-kill_connection_manager (gpointer data)
-{
-  if (!g_getenv ("IDLE_PERSIST") && !connections_exist)
-    {
-      g_debug("no connections, and timed out");
-      g_object_unref (manager);
-      g_main_loop_quit (mainloop);
-    }
-
-  return FALSE;
-}
-
-static void
-new_connection (IdleConnectionManager *conn, gchar *bus_name,
-                gchar *object_path, gchar *proto)
-{
-  	g_debug("%s called with %s, %s, %s", G_STRFUNC, bus_name, object_path, proto);
-  connections_exist = TRUE;
-  g_source_remove (timeout_id);
-}
-
-static void
-no_more_connections (IdleConnectionManager *conn)
-{
-  if (g_main_context_find_source_by_id (g_main_loop_get_context (mainloop),
-                                       timeout_id))
-    {
-      g_source_remove (timeout_id);
-    }
-  connections_exist = FALSE;
-  timeout_id = g_timeout_add(DIE_TIME, kill_connection_manager, NULL);
+	return base_cm;
 }
 
 int main(int argc, char **argv) {
+	if (g_getenv("IDLE_DEBUG"))
+		tp_debug_set_flags_from_env("IDLE_DEBUG");
 
-  g_type_init();
-  
-  g_set_prgname("telepathy-idle");
+	if (g_getenv("IDLE_PERSIST"))
+		tp_debug_set_all_flags();
 
-  mainloop = g_main_loop_new (NULL, FALSE);
-
-  dbus_g_error_domain_register (TP_ERRORS, "org.freedesktop.Telepathy.Error", TP_TYPE_ERROR);
-
-  manager = g_object_new (IDLE_TYPE_CONNECTION_MANAGER, NULL);
-
-  g_signal_connect (manager, "new-connection",
-                    (GCallback) new_connection, NULL);
-
-  g_signal_connect (manager, "no-more-connections",
-                    (GCallback) no_more_connections, NULL);
-
-  tp_base_connection_manager_register(TP_BASE_CONNECTION_MANAGER(manager));
-
-  g_debug ("started");
-
-  timeout_id = g_timeout_add(DIE_TIME, kill_connection_manager, NULL);
-
-  g_main_loop_run (mainloop);
-
-  return 0;
+	return tp_run_connection_manager("telepathy-idle", VERSION, _construct_cm, argc, argv);
 }
+
