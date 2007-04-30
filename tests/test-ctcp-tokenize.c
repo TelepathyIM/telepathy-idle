b1faee39e9cba4d4f4eda0b30967ca837904e562
Olli Salli <olli.salli@collabora.co.uk>
2007-04-30 14:24:13 +0000

    Make test-ctcp-tokenize return 1 on failure
    20070430142413-9db4d-2f076672218ebde01d4134b3eca22c8556e7652f.gz

--- tests/test-ctcp-tokenize.c
+++ tests/test-ctcp-tokenize.c
@@ -7,15 +7,22 @@
 #include <telepathy-glib/util.h>
 
 int main() {
+	gboolean fail = FALSE;
+
 	const gchar *test_str = "\001  foo \" fo bar\" bar\\001\\002\\003baz\"for every foo there is \\001 bar\\\\\001";
 	gchar **tokens = idle_ctcp_decode(test_str);
 
 	const gchar *should_be[] = {"foo", " fo bar", "bar\001\002\003baz", "for every foo there is \001 bar\\", NULL};
 	for (int i = 0; i < (sizeof(should_be) / sizeof(should_be[0])); i++) {
-		if (tp_strdiff(tokens[i], should_be[i]))
+		if (tp_strdiff(tokens[i], should_be[i])) {
 			fprintf(stderr, "Should be \"%s\", is \"%s\"\n", g_strescape(should_be[i], NULL), g_strescape(tokens[i], NULL));
+			fail = TRUE;
+		}
 	}
 
-	return 0;
+	if (fail)
+		return 1;
+	else
+		return 0;
 }
 
