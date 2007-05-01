c042b96cbd636f9e927dd3aba2898a5428983ea4
Olli Salli <olli.salli@collabora.co.uk>
2007-05-01 11:17:01 +0000

    Add minimal test for idle_ctcp_kill_blingbling
    20070501111701-9db4d-7c0d921bfc5795eef541443157bfdfada0faf1a3.gz

---
+++ tests/test-ctcp-kill-blingbling.c
@@ -0,0 +1,31 @@
+#include <idle-ctcp.h>
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	gboolean fail = FALSE;
+
+	const gchar *test_strings[] = {
+		"foobar", "foobar",
+		"foo \x03\x31\x33<3", "foo <3",
+		NULL, NULL
+	};
+
+	for (int i = 0; test_strings[i] != NULL; i += 2) {
+		gchar *killed = idle_ctcp_kill_blingbling(test_strings[i]);
+
+		if (strcmp(killed, test_strings[i + 1])) {
+			fprintf(stderr, "\"%s\" -> \"%s\", should be \"%s\"", test_strings[i], killed, test_strings[i + 1]);
+			fail = TRUE;
+		}
+
+		g_free(killed);
+	}
+
+	if (fail)
+		return 1;
+	else
+		return 0;
+}
+
