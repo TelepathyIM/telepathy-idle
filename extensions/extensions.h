cd911ed48753313a8faf0914cfc280a3379947fe
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-09-25 19:28:41 +0000

    Add enough extensions-generating infrastructure to generate stubs for the Renaming interface
    20070925192841-53eee-a61557a2cf97d52ca9dfb909cf2659d2ff9a165c.gz

---
+++ extensions/extensions.h
@@ -0,0 +1,10 @@
+
+#ifndef _IDLE_EXTENSIONS_H
+#define _IDLE_EXTENSIONS_H
+
+#include "extensions/_gen/svc.h"
+#include "extensions/_gen/enums.h"
+#include "extensions/_gen/interfaces.h"
+
+#endif /* _IDLE_EXTENSIONS_H */
+
