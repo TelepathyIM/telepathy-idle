1ce34440c58a9b82cb15711d95e35a8acb512ecd
Will Thompson <will.thompson@collabora.co.uk>
2012-07-18 14:12:07 +0100

    servicetest: squash dbus-python deprecation warning

--- tests/twisted/servicetest.py
+++ tests/twisted/servicetest.py
@@ -11,7 +11,9 @@ import sys
 import pprint
 import unittest
 
-import dbus.glib
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+DBusGMainLoop(set_as_default=True)
 
 from twisted.internet import reactor
 
