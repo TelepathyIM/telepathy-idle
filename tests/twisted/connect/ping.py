2667606678abd63820b321d3d51ebd0efc0c3e24
Will Thompson <will.thompson@collabora.co.uk>
2012-10-30 14:20:43 +0000

    Make the PONGs sent back by the test more realistic

--- tests/twisted/connect/ping.py
+++ tests/twisted/connect/ping.py
@@ -3,15 +3,17 @@ Test Idle sending PINGs.
 """
 
 from idletest import exec_test
-from servicetest import EventPattern, call_async
+from servicetest import assertLength
 
 def test(q, bus, conn, stream):
     conn.Connect()
     q.expect('dbus-signal', signal='StatusChanged', args=[0, 1])
 
     e = q.expect('stream-PING')
+    assertLength(1, e.data)
     timestamp = e.data[0]
-    stream.sendMessage('PONG', timestamp)
+    stream.sendMessage('PONG', 'idle.test.server', ':%s' % timestamp,
+        prefix='idle.test.server')
 
 if __name__ == '__main__':
     exec_test(test, params={
